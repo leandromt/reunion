79cce4342ebaba1cf578452b4769fe8426ca6551
Marcellus Frota <marcellusfrota@gmail.com>
2018-07-16 20:52:56 -0300

    Prepare bootstrap files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import './scss/base.scss'
+// import '../node_modules/bootstrap/scss/_mixins.scss'
+import '../node_modules/bootstrap/scss/_functions.scss'
+import '../node_modules/bootstrap/scss/_variables.scss'
+// import '../node_modules/bootstrap/scss/bootstrap-grid.scss'
+// import '../node_modules/bootstrap/scss/_forms.scss'
+// import './scss/base.scss'
 
 // import './js/rainbows.js';
 // import './js/unicorns.js';
